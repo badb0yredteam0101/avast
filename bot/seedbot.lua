8a3c9c7fda0e1cac54ad7e7e17c7bef7d61eaaef
badb0yredteam0101 <saiedlore.63@gmail.com>
2016-05-24 18:23:30 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -327,98 +327,98 @@ This command will send text to [group_id]
 Commands list :
 
 !kick [username|id]
-You can also do it by reply
-
+You can also do it by reply ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !ban [ username|id]
-You can also do it by reply
-
+You can also do it by reply ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !unban [id]
-You can also do it by reply
-
+You can also do it by reply ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !who
-Members list
-
+Members list ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !modlist
-Moderators list
-
+Moderators list ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !promote [username]
-Promote someone
-
+Promote someone ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !demote [username]
-Demote someone
-
+Demote someone ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !kickme
-Will kick user
-
+Will kick user ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !about
-Group description
-
+Group description ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !setphoto
-Set and locks group photo
-
+Set and locks group photo ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !setname [name]
-Set group name
-
+Set group name ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !rules
-Group rules
-
+Group rules ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !id
-Return group id or user id
-
+Return group id or user id ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !help
-Get commands list
-
+Get commands list➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !lock [member|name|bots|leave] 
-Locks [member|name|bots|leaveing] 
-
+Locks [member|name|bots|leaveing] ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !unlock [member|name|bots|leave]
-Unlocks [member|name|bots|leaving]
-
+Unlocks [member|name|bots|leaving]➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !set rules [text]
-Set [text] as rules
-
+Set [text] as rules ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !set about [text]
-Set [text] as about
-
+Set [text] as about ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !settings
-Returns group settings
-
+Returns group settings ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !newlink
-Create/revoke your group link
-
+Create/revoke your group link ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !link
-Returns group link
-
+Returns group link ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !owner
-Returns group owner id
-
+Returns group owner id ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !setowner [id]
-Will set id as owner
-
+Will set id as owner ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !setflood [value]
-Set [value] as flood sensitivity
-
+Set [value] as flood sensitivity ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !stats
-Simple message statistics
-
+Simple message statistics ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !save [value] [text]
-Save [text] as [value]
-
+Save [text] as [value] ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !get [value]
-Returns text of [value]
-
+Returns text of [value] ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
-
+Will clear [modlist|rules|about] and set it to nil ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !res [username]
-Returns user id
-
+Returns user id ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !log
-Will return group logs
-
+Will return group logs ➯
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 !banlist
-Will return group ban list
-
+Will return group ban list ➱
+﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 » U can use both "/" and "!" 
 
 » Only mods, owner and admin can add bots in group
@@ -426,7 +426,8 @@ Will return group ban list
 » Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 
 » Only owner can use res,setowner,promote,demote and log commands
-
+@redteam_01_01
+@redTM_BOT
 ]]
   }
   serialize_to_file(config, './data/config.lua')
